eb08dde4771ee7a5be5e70915ea216400e21858f
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-06 22:26:02 +0100

    add: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::{serve, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/hello", get(|| async { "hello" }));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    serve(listener, router).await.unwrap();
 }
