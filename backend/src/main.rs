dd7af3c091d9627bd0eb0f14c2ee8a7dca9995d4
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-08 15:31:58 +0100

    add(backend): dummy route + permissive cors layers

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,18 @@
 use axum::routing::get;
-use axum::{serve, Router};
+use axum::{serve, Json, Router};
 use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
+
+async fn get_current_temperature() -> Json<f32> {
+    Json(30.0)
+}
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/hello", get(|| async { "hello" }));
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let router = Router::new()
+        .route("/hello", get(|| async { "hello" }))
+        .route("/current", get(get_current_temperature))
+        .layer(CorsLayer::permissive());
+    let listener = TcpListener::bind("0.0.0.0:9999").await.unwrap();
     serve(listener, router).await.unwrap();
 }
