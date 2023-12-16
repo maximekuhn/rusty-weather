06fd9daca3d8485a6358889373ba8d8d59c5d8a5
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-16 18:45:53 +0100

    add(backend): actually getting data from open weather

--- backend/src/weather/model.rs
+++ backend/src/weather/model.rs
@@ -24,24 +24,54 @@ pub struct ForecastWeather {
 #[derive(Deserialize)]
 pub struct OpenWeatherAPICurrent {
     weather: Vec<OpenWeatherAPICurrentWeather>,
+    main: OpenWeatherAPICurrentMain,
+    wind: OpenWeatherAPICurrentWind,
+    sys: OpenWeatherAPICurrentSys,
+    visibility: f32,
 }
 
 #[derive(Deserialize)]
-pub struct OpenWeatherAPICurrentWeather {}
+pub struct OpenWeatherAPICurrentWeather {
+    description: String,
+}
+
+#[derive(Deserialize)]
+pub struct OpenWeatherAPICurrentMain {
+    temp: f32,
+    feels_like: f32,
+    pressure: f32,
+    humidity: f32,
+}
+
+#[derive(Deserialize)]
+pub struct OpenWeatherAPICurrentWind {
+    speed: f32,
+}
+
+#[derive(Deserialize)]
+pub struct OpenWeatherAPICurrentSys {
+    sunset: i64,
+    sunrise: i64,
+}
 
 impl From<OpenWeatherAPICurrent> for CurrentDayWeather {
     fn from(current_weather: OpenWeatherAPICurrent) -> Self {
+        let description = match current_weather.weather.first() {
+            None => "",
+            Some(weather) => &weather.description,
+        };
+
         Self {
-            temperature: 0.0,
-            feels_like: 0.0,
-            pressure: 0.0,
-            humidity_rate: 0.0,
-            wind_speed: 0.0,
-            visibility: 0.0,
+            temperature: current_weather.main.temp,
+            feels_like: current_weather.main.feels_like,
+            pressure: current_weather.main.pressure,
+            humidity_rate: current_weather.main.humidity,
+            wind_speed: current_weather.wind.speed,
+            visibility: current_weather.visibility,
             probability_of_precipitation: 0.0,
-            sunset: 0,
-            sunrise: 0,
-            description: "TODO".to_string(),
+            sunset: current_weather.sys.sunset,
+            sunrise: current_weather.sys.sunrise,
+            description: description.to_string(),
         }
     }
 }
