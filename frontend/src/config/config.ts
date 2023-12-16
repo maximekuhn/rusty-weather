5ff61a77e7601acbbe833d5888020427911e6eca
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-16 23:29:57 +0100

    add(frontend): qrcode + button to redirect to settings

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -2,16 +2,22 @@ import {Language} from "./SettingsContext";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 let backendURL;
+let settingsURL;
 if (isDevelopment) {
     // assumes rust backend is up and running
     backendURL = "http://localhost:9999/api";
+
+    // change this to your local IP address
+    settingsURL = "http://192.168.1.19:3000/settings";
 } else {
     // assumes rust backend is running on the same machine as frontend nginx
     backendURL = "/api";
+    settingsURL = "/settings";
 }
 
 export const config = {
     BACKEND_URL: backendURL,
+    SETTINGS_URL: settingsURL,
     DEFAULT_CITY: "Paris",
     DEFAULT_LANGUAGE: Language.English,
 };
