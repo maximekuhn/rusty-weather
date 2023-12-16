b1ee18bed6791e75ab78e8f0e8d7611586fb8c39
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-16 22:54:31 +0100

    add(frontend): navbar clock + city

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import NavbarItem from "./NavbarItem";
 import {useState} from "react";
 import './Navbar.css';
+import {useSettings} from "../../config/SettingsContext";
+import NavbarClock from "./NavbarClock";
 
 function Navbar() {
+    const {settings} = useSettings();
     const [activeItem, setActiveItem] = useState<string>("home");
 
     function handeItemClick(title: string) {
@@ -11,12 +14,14 @@ function Navbar() {
 
     return (
         <div className="Navbar">
+            <h1>{settings.city}</h1>
             <NavbarItem title={"home"} pathTo={"/"} isActive={activeItem === "home"}
                         onClick={() => handeItemClick("home")}/>
             <NavbarItem title={"forecast"} pathTo={"/forecast"} isActive={activeItem === "forecast"}
                         onClick={() => handeItemClick("forecast")}/>
             <NavbarItem title={"settings"} pathTo={"/settings"} isActive={activeItem === "settings"}
                         onClick={() => handeItemClick("settings")}/>
+            <NavbarClock />
         </div>
     );
 }
