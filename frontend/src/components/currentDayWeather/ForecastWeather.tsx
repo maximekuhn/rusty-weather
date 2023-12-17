a79525bfe0828b066ba8d1f37cd3f891d4aedfd1
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-17 18:27:26 +0100

    add(frontend): current weather

---
+++ frontend/src/components/currentDayWeather/ForecastWeather.tsx
@@ -0,0 +1,11 @@
+import './CurrentDayWeather.css';
+
+function ForecastWeather() {
+    return(
+        <div className="ForecastWeather">
+            <p>forecast weather</p>
+        </div>
+    );
+}
+
+export default ForecastWeather;
