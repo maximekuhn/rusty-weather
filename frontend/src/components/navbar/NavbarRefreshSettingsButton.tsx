74b6e5a334f36b7a3084776b2c8a96eea8ff0d88
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-17 18:30:17 +0100

    add(frontend): navbar refresh settings actually refresh settings

--- frontend/src/components/navbar/NavbarRefreshSettingsButton.tsx
+++ frontend/src/components/navbar/NavbarRefreshSettingsButton.tsx
@@ -1,9 +1,22 @@
-import { FiRefreshCcw } from "react-icons/fi";
+import {FiRefreshCcw} from "react-icons/fi";
+import {getCurrentSettings} from "../../api/settings";
+import {useSettings} from "../../config/SettingsContext";
 
 
 function NavbarRefreshSettingsButton() {
-    return(
-        <FiRefreshCcw />
+    const {settings, setSettings} = useSettings();
+
+    function queryAndUpdateSettings() {
+        getCurrentSettings().then((currentSettings) => {
+            setSettings({
+                city: currentSettings.current_city, language: settings.language
+
+            });
+        }).catch((err) => console.error(`Something went wrong: ${err}`));
+    }
+
+    return (
+        <FiRefreshCcw onClick={queryAndUpdateSettings}/>
     );
 }
 
