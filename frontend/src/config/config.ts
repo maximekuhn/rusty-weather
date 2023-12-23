34c3d1a0e6081757eb01aca818bf3458d564ac7f
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-12-23 12:01:40 +0100

    fix(frontend): typo in comment

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -9,7 +9,7 @@ if (isDevelopment) {
     backendURL = "http://192.168.1.19:9999/api";
     settingsURL = "http://192.168.1.19:3000/settings";
 } else {
-    // assumes rust backend is running on the same machine as frontend nginx
+    // assumes rust backend is running on the same machine as the nginx frontend
     backendURL = `http://${window.location.hostname}:${window.location.port}/api`;
     settingsURL = `http://${window.location.hostname}:${window.location.port}/settings`;
 }
